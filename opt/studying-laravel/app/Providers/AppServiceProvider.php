a5582cc5a9500de476cbb359ef5e83279712f508
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-10-19 12:32:44 +0900

    Laravel Duskの導入(途中、あとでやる)

--- opt/studying-laravel/app/Providers/AppServiceProvider.php
+++ opt/studying-laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(DuskServiceProvider::class);
         }
     }
 }
